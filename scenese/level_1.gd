43c1bfb353572405caaa98389c8782730fc4204f
sigdelaryab-cmd <sigdelaryab@gmail.com>
2026-02-15 13:47:08 -0600

    Background Music
    Added Background Music for Levels 1 and 2

--- scenese/level_1.gd
+++ scenese/level_1.gd
@@ -1,8 +1,11 @@
 extends Node2D
 
 func _ready():
-	$Node2d.show_instruction("Press A/D to move")
+	print("Level 1 _ready ran")
+	$Instr_1.show_instruction("Press A/D to move")
+	Music.play_track(preload("res://audio/level1.ogg"))
+	print("Music singleton is:", Music)
 	
 	
 func _on_lvl_1_2_body_entered(PhysicsBody2D) -> void:
-	$Node2d.show_instruction("Press E to move to the next level")
+	$Instr_1.show_instruction("Press E to move to the next level")
--- scenese/level_3.gd
+++ scenese/level_3.gd
@@ -3,4 +3,5 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$instr_3.show_instruction("Use L-shift to defend. Use LMB to attack ")
+	$Instr_3.show_instruction("Use L-shift to defend. Use LMB to attack ")
+	Music.stop_music()
